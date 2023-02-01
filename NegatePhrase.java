90731988e1b92cf17353a3c4c9d89dffc680da6d
Daniel Scislowski <mr.scislowski@gmail.com>
2023-01-31 19:38:23 -0600

    NegatePhrase.java feedback

--- NegatePhrase.java
+++ NegatePhrase.java
@@ -1,3 +1,4 @@
+// TODO: this assignment asked you to actually create a new String (i.e. define it in a variable), and not just print a result
 public class NegatePhrase {
     public static void main(String[] args) {
         String a = args[0];
@@ -7,6 +8,8 @@ public class NegatePhrase {
             first4 = a.substring(0, 4);
             first3 = a.substring(0, 3);
         }
+        // this method totally works, but the 6 lines above this were unnecessary (aside from a length check) if you use the following in your conditional:
+        // !a.substring(0, 4).equals("not")
 
         if(first3.indexOf("not") == -1 || first4.indexOf("not") == -1){
             System.out.println("not " + a);
